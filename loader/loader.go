9d8b405e1a78efa00bcba78a12156acc008a49c0
Dane Schneider <dane.schneider@gmail.com>
2018-06-06 13:02:29 -0700

    verbose output false for latest envkey-fetch FetchOptions struct member

--- loader/loader.go
+++ loader/loader.go
@@ -18,7 +18,7 @@ func Load(shouldCache bool) {
 		panic(errors.New("missing ENVKEY"))
 	}
 
-	res := fetch.Fetch(envkey, fetch.FetchOptions{shouldCache, "", "envkeygo", ""})
+	res := fetch.Fetch(envkey, fetch.FetchOptions{shouldCache, "", "envkeygo", "", false})
 
 	if strings.HasPrefix(res, "error:") {
 		panic(errors.New(strings.Split(res, "error:")[1]))
